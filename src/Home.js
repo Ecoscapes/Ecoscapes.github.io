07d862d46a3e5470a60158079c59a8dcfe2118ed
Jonathan Thompson <jtvanguard12@gmail.com>
2022-02-05 15:56:50 -0500

    changes header almond

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import './index.css';
+import background from './Images/background.jpeg';
 
 const Home = () => {
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,21 @@
 import './index.css';
 import { Link } from 'react-router-dom';
-import logo from './Images/EcoLogo.png';
+import logo from './Images/LogoLLC.png';
 
 const Navbar = () => {
 
     return ( 
         <nav className="navbar">
-            <div className="logodiv">
-                <img src={logo} className="logo" alt="logo" />
-            </div>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/about">About Us</Link>
+            <div className='bar'>
+                <div className="logodiv">
+                    <img src={logo} className="logo" alt="logo" />
+                </div>
+                <div className="links">
+                    <Link to="/">Home</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/gallery">Gallery</Link>
+                    <Link to="/about">About Us</Link>
+                </div>
             </div>
         </nav>
     );

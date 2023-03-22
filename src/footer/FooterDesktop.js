4faeb455d22bd50e6c5b43926ced9b3718317bf2
Jonathan Thompson <jtvanguard12@gmail.com>
2023-03-22 17:38:16 -0400

    footer button for desktop

--- src/footer/FooterDesktop.js
+++ src/footer/FooterDesktop.js
@@ -1,31 +1,40 @@
-import './footerDesktop.module.css';
+import styles from  './footerDesktop.module.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import isa from '../Images/ISA.png';
 
 const FooterDesktop = () => {
+    const navigate = useNavigate();
+
+    const handleContactClick = () => {
+        navigate("/contact");
+        
+    }
 
     return ( 
         <div>
-            <nav className="footer">
-                <div className='footerbar'>
-                    <div className='isaimage'>
-                        <img src={isa} className="isa" alt="insta logo" />
-                    </div>
-                    <div className='footertextcontainer'>
-                        <div className='footertext'>
-                            ISA # MA-6060-A / MD LTE# 2208
+            <nav className={styles.footercontainer}> 
+                <div className={styles.footerbuttoncontainer}>
+                    <button className={styles.footerbutton}  onClick={handleContactClick}>Contact Us</button>
+                </div>
+                <div className={styles.footer}>
+                    <div  className={styles.footerbar}>
+                        <div  className={styles.isaimage}>
+                            <img src={isa}  className={styles.isa} alt="insta logo" />
                         </div>
-                        <div className='footertext'>
-                            Contractors MHIC # 142765
-                        </div>
-                    </div>    
-                    <div className='footertextcontainer'>
-                        <div className='footertext'>
-                            Contact Us
-                        </div>
-                        <div className='footertext'>
-                            Come Work With Us
+                        <div  className={styles.footertextcontainer}>
+                            <div  className={styles.footertext}>
+                                ISA # MA-6060-A / MD LTE# 2208
+                            </div>
+                            <div  className={styles.footertext}>
+                                Contractors MHIC # 142765
+                            </div>
+                            <div  className={styles.footertext} >
+                                <div className={styles.footerwork} onClick={handleContactClick}>
+                                    Come Work With Us
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
